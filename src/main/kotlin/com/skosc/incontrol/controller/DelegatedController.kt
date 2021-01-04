31184beec1e157ac7b23cc164492d6a89c5426ab
Alexey <alexey.yakovlef@gmail.com>
2021-01-04 07:26:35 -0800

    - added some handler validation

--- src/main/kotlin/com/skosc/incontrol/controller/DelegatedController.kt
+++ src/main/kotlin/com/skosc/incontrol/controller/DelegatedController.kt
@@ -1,5 +1,6 @@
 package com.skosc.incontrol.controller
 
+import com.skosc.incontrol.handler.validator.HandlerValidator
 import com.skosc.incontrol.reflect.ControllerHandlerMethod
 import com.skosc.incontrol.reflect.HandlerMethodFinder
 import io.ktor.application.*
@@ -16,8 +17,12 @@ internal class DelegatedController(private val controller: Controller) {
 
     private val handlerMethodFinder: HandlerMethodFinder = HandlerMethodFinder()
     private val parameterRetriever: ControllerParameterRetriever = ControllerParameterRetriever()
-    private val handler = handlerMethodFinder.findHandlerMethod(controller)
-    private val delegatedHandler: ControllerHandlerMethod = ControllerHandlerMethod(handler)
+    private val delegatedHandler: ControllerHandlerMethod = ControllerHandlerMethod(handlerMethodFinder.findHandlerMethod(controller))
+    private val handlerValidator: HandlerValidator = HandlerValidator()
+
+    init {
+        handlerValidator.validateOrThrow(delegatedHandler)
+    }
 
     suspend fun handle(call: ApplicationCall) {
         val parameters = parameterRetriever.retrieveParameters(delegatedHandler.parameters, call)
---
+++ src/main/kotlin/com/skosc/incontrol/handler/validator/ParameterValidator.kt
@@ -0,0 +1,41 @@
+package com.skosc.incontrol.handler.validator
+
+import com.skosc.incontrol.handler.ParameterType
+import com.skosc.incontrol.reflect.ControllerHandlerParameter
+import java.lang.IllegalArgumentException
+import kotlin.reflect.KType
+import kotlin.reflect.full.createType
+
+/**
+ * Class containing validation logic for [ControllerHandlerParameter]
+ *
+ * @author a.yakovlev
+ * @since indev
+ */
+internal class ParameterValidator {
+
+    /**
+     * Valid types for [ParameterType.QUERY] and [ParameterType.PATH]
+     */
+    private val validPathAndQueryParameterTypes: Set<KType> = setOf(
+        String::class.createType(),
+        Int::class.createType(),
+        Double::class.createType(),
+        Boolean::class.createType()
+    )
+
+    /**
+     * Validates [parameter], throws [IllegalArgumentException] if it is invalid
+     */
+    fun validateOrThrow(parameter: ControllerHandlerParameter) = when (parameter.type) {
+        ParameterType.BODY -> Unit
+        ParameterType.QUERY,
+        ParameterType.PATH -> validatePathOrQuery(parameter)
+    }
+
+    private fun validatePathOrQuery(parameter: ControllerHandlerParameter) = when {
+        parameter.kType !in validPathAndQueryParameterTypes ->
+            throw IllegalArgumentException("Unsupported parameter type: ${parameter.type}")
+        else -> Unit
+    }
+}
