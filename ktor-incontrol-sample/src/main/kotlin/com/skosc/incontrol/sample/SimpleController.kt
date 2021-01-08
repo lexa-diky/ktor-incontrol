e9a26eb9714b3752197cb822c192817d4edf953c
Alexey <alexey.yakovlef@gmail.com>
2021-01-08 10:43:29 -0800

    - added sample project - added kodein integration project

---
+++ ktor-incontrol-sample/src/main/kotlin/com/skosc/incontrol/sample/SimpleController.kt
@@ -0,0 +1,22 @@
+package com.skosc.incontrol.sample
+
+import com.skosc.incontrol.InControl
+import com.skosc.incontrol.annotation.Query
+import com.skosc.incontrol.controller.Controller
+import com.skosc.incontrol.routing.get
+import io.ktor.application.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        install(Routing)
+        install(InControl)
+        routing {
+            get(object : Controller {
+                suspend fun handle(@Query name: String = "World") = "Hello $name"
+            })
+        }
+    }.start()
+}
